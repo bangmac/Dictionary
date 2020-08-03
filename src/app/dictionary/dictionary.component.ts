8e100a33d4c7a8c413aaa68efcd8c6b14a174eab
bangmac <anhbexd@gmail.com>
2020-08-03 16:30:16 +0700

    add service

--- src/app/dictionary/dictionary.component.ts
+++ src/app/dictionary/dictionary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DictionaryService, IWord } from '../dictionary.service';
 
 @Component({
   selector: 'app-dictionary',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dictionary.component.scss']
 })
 export class DictionaryComponent implements OnInit {
+  word: IWord;
+  constructor(private dictionaryService: DictionaryService) { }
 
-  constructor() { }
+  ngOnInit() {
+  }
 
-  ngOnInit(): void {
+  search(word: string) {
+    const meaning = this.dictionaryService.search(word);
+    this.word = {
+      key: word,
+      meaning: meaning
+    };
   }
 
 }
